f94cefd4206f227ac39f807992404efa4ea92a24
Gabriel Ramos <luisgabrielramosrobles@gmail.com>
2025-08-09 15:59:18 -0400

    Small fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,19 +17,6 @@ app.use(
 
 app.use(express.json())
 
-// TODO: Jhonnatan: Revisar applicacion de los controladores con paginacion en las rutas
-// TODO: Refactorizar router en archivos separados
-// Ruta de autenticación y autorización (sin permiso)
-app.use('/auth', AuthRouter)
-
-// Rutas de usuarios, roles y permisos (con permisos)
-app.use(authenticateToken)  
-
-app.use('/users', UserRouter)
-app.use('/permissions', PermissionRouter)
-app.use('/roles', RoleRouter)
-
-// TODO: Implementar todas las demas rutas dentro de este main router
 app.use('/api', mainRouter)
 
 export default app
